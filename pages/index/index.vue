9431471812b96e8b62d5d359dd8db83c8ea0cdf4
Xavier <admin@54xavier.cn>
2024-07-15 13:57:39 +0800

    init project

---
+++ pages/index/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <view class="container">
+    <view class="info">
+      这是一个 uniapp 开发的 vue-plugin-hiprint 周边生态项目，实现了 uniapp
+      IOS、Android 中的打印。
+    </view>
+    <uv-button type="primary" text="进入打印 webView 页" @click="navigator"></uv-button>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  onLoad() {},
+  methods: {
+    navigator() {
+      uni.navigateTo({
+        url: "/pages/webview/index",
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  box-sizing: border-box;
+  color: $uv-main-color;
+}
+.info {
+  margin: 16px 0;
+}
+</style>
